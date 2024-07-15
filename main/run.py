75735c80a15a26fd01d809877a8adcc8a0a9bbd5
Shivam Nagar <162788327+ShivamNagar2002@users.noreply.github.com>
2024-07-15 17:05:02 +0530

    Update run.py
    - Replaced basic IF-ELSE statements

--- main/run.py
+++ main/run.py
@@ -19,29 +19,10 @@ import threading
 
 
 toblk = []
-sv_list = ["maa", "maa2", "bom", "bom2", "dxb", "sgp", "seo", "ams", "atl", "fra", "lhr", "par", "mad", "dfw", "jfk", "iad", "sea", "canm", "ctum", "sham", "tsnm" ]
 
 currBlocked = [] 
 alldata = []
 counter = 0
-
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -49,7 +30,7 @@ class MainWindow(QMainWindow):
     global toblk 
     toblk = []
     global sv_list 
-    sv_list = ["maa", "maa2", "bom", "bom2", "dxb", "sgp", "seo", "ams", "atl", "fra", "lhr", "par", "mad", "dfw", "jfk", "iad", "sea", "canm", "ctum", "sham", "tsnm" ]
+    sv_list = [ "maa2", "bom2", "dxb", "sgp", "seo", "ams", "atl", "fra", "lhr", "par", "mad", "dfw", "jfk", "iad", "sea", "canm", "ctum", "sham", "tsnm" ]
     global currBlocked 
     currBlocked = [] 
     global alldata
@@ -64,11 +45,11 @@ class MainWindow(QMainWindow):
         ## calling on load 
         t1 = threading.Thread(target=self.onload)
         t1.start()
-        self.ui.pushButton_4.clicked.connect(lambda: os.system("start https://github.com/ShivamNagar2002") )
+        self.ui.pushButton_3.clicked.connect(lambda: os.system("start https://github.com/ShivamNagar2002") )
        
         
-        self.ui.pushButton.clicked.connect(self.startbtn)
-        self.ui.pushButton_3.clicked.connect(self.unblock)
+        self.ui.pushButton.clicked.connect(self.newFncStart)
+        self.ui.pushButton_4.clicked.connect(self.unblock)
         self.ui.pushButton_2.clicked.connect(self.f_exit)
         
        
@@ -84,6 +65,12 @@ class MainWindow(QMainWindow):
             os._exit()
             exit()
 
+
+
+
+
+## Function to unblock all Servers 
+
     def unblock(self):
         global toblk, sv_list , alldata
         for tmpx in sv_list :
@@ -94,156 +81,105 @@ class MainWindow(QMainWindow):
             except:
                 pass
         
-        self.ui.checkBox.setCheckState(Qt.Unchecked)
-        self.ui.checkBox_2.setCheckState(Qt.Unchecked)
-        self.ui.checkBox_3.setCheckState(Qt.Unchecked)
-        self.ui.checkBox_4.setCheckState(Qt.Unchecked)
-        self.ui.checkBox_5.setCheckState(Qt.Unchecked)
-        self.ui.checkBox_6.setCheckState(Qt.Unchecked)
-        self.ui.checkBox_7.setCheckState(Qt.Unchecked)
-        self.ui.checkBox_8.setCheckState(Qt.Unchecked)
-        self.ui.checkBox_9.setCheckState(Qt.Unchecked)
-        self.ui.checkBox_10.setCheckState(Qt.Unchecked)
-        self.ui.checkBox_11.setCheckState(Qt.Unchecked)
-        self.ui.checkBox_12.setCheckState(Qt.Unchecked)
-        self.ui.checkBox_13.setCheckState(Qt.Unchecked)
-        self.ui.checkBox_14.setCheckState(Qt.Unchecked)
-        self.ui.checkBox_15.setCheckState(Qt.Unchecked)
-        self.ui.checkBox_16.setCheckState(Qt.Unchecked)
-        self.ui.checkBox_17.setCheckState(Qt.Unchecked)
-        self.ui.checkBox_18.setCheckState(Qt.Unchecked)
-        self.ui.checkBox_19.setCheckState(Qt.Unchecked)
-        self.ui.checkBox_20.setCheckState(Qt.Unchecked)
-        self.ui.checkBox_21.setCheckState(Qt.Unchecked)
-  
+        checkboxes = self.ui.mainFrame.findChildren(QCheckBox)
+        ## Changing staes of all checkboxes 
+        for checkbox in checkboxes:
+            checkbox.setCheckState(Qt.Unchecked)
+
+
+     
         
         
         
 
-
-
-
-    def block(self):
-        global toblk, sv_list , alldata, currBlocked
-
-        for tmpx in sv_list:
-            if tmpx not in toblk:
-                cmd = "netsh advfirewall firewall show rule name=CS2ServerSelect_BLOCK_{0} >NUL 2>&1".format(tmpx)
-                if os.system(cmd) != 1:
-                    try:
-                        cmd = "netsh advfirewall firewall delete rule name=CS2ServerSelect_BLOCK_{0}  >NUL 2>&1".format(tmpx)
-                        os.system(cmd)
-                    except:
-                        pass
-
-
-        for tmpx in toblk :
-            x = 0
-            x2 = ""
-            while x < len(alldata[tmpx]):
-                
-                tmpvar = "CS2ServerSelect_BLOCK_"+tmpx
-                
-                if x == 0:
-                    x2 = x2 + alldata[tmpx][x]
-                if x > 0:
-                    x2 = x2 + "," + alldata[tmpx][x]
-                x= x+1
-            cmd = "netsh advfirewall firewall show rule name=CS2ServerSelect_BLOCK_{0} >NUL 2>&1".format(tmpx)
-            if os.system(cmd) == 1:
-                try:
-                    cmd = "netsh advfirewall firewall add rule  name={0}  dir=out action=block protocol=ANY  remoteip= \"{1}\" >NUL 2>&1".format(tmpvar, x2)
-                    os.system(cmd)
-                except:
-                    pass
-
-                
-
-
-
-
-
-
-
-
-
-
-
-
-    def startbtn(self):
-        global toblk
-        toblock  = []
-        if int(self.ui.checkBox.checkState()) == 2:
-            toblock.append("bom")
-        if int(self.ui.checkBox_2.checkState()) == 2:
-            toblock.append("bom2")
-        if int(self.ui.checkBox_3.checkState()) == 2:
-            toblock.append("maa")
-        if int(self.ui.checkBox_4.checkState()) == 2:
-            toblock.append("maa2")
-        if int(self.ui.checkBox_5.checkState()) == 2:
-            toblock.append("canm")
-        if int(self.ui.checkBox_6.checkState()) == 2:
-            toblock.append("ctum")
-        if int(self.ui.checkBox_7.checkState()) == 2:
-            toblock.append("sham")
-        if int(self.ui.checkBox_8.checkState()) == 2:
-            toblock.append("tsnm")
-        if int(self.ui.checkBox_9.checkState()) == 2:
-            toblock.append("ams")
-        if int(self.ui.checkBox_10.checkState()) == 2:
-            toblock.append("atl")
-        if int(self.ui.checkBox_11.checkState()) == 2:
-            toblock.append("fra")
-        if int(self.ui.checkBox_12.checkState()) == 2:
-            toblock.append("lhr")
-        if int(self.ui.checkBox_13.checkState()) == 2:
-            toblock.append("par")
-        if int(self.ui.checkBox_14.checkState()) == 2:
-            toblock.append("mad")
-        if int(self.ui.checkBox_15.checkState()) == 2:
-            toblock.append("dfw")
-        if int(self.ui.checkBox_16.checkState()) == 2:
-            toblock.append("jfk")
-        if int(self.ui.checkBox_17.checkState()) == 2:
-            toblock.append("iad")
-        if int(self.ui.checkBox_18.checkState()) == 2:
-            toblock.append("sea")
-        if int(self.ui.checkBox_19.checkState()) == 2:
-            toblock.append("dxb")
-        if int(self.ui.checkBox_20.checkState()) == 2:
-            toblock.append("sgp")
-        if int(self.ui.checkBox_21.checkState()) == 2:
-            toblock.append("seo")
-        
-        toblk = set(toblock)
-        self.block()
      
 
+### New Function to block Selected servers ( Removed old if checks )
+    def newBlockFnc(self):
+        global toblk, alldata
+        for key in toblk.keys():
+            firewall_rule_name = "CS2ServerSelect_BLOCK_"+ key
+            if toblk[key] == True:
+                x = 0 
+                x2 = ""
+                while x < len(alldata[key]):
+                    if x == 0 :
+                        x2 = x2 + alldata[key][x]
+                    if x > 0:
+                        x2 = x2 + ',' + alldata[key][x]
+                    x = x + 1
+                cmd = "netsh advfirewall firewall show rule name={0} >NUL 2>&1".format(firewall_rule_name)
+                
+                if os.system(cmd) == 1:
+                    try:
+                        cmd = "netsh advfirewall firewall add rule  name={0}  dir=out action=block protocol=ANY  remoteip= \"{1}\"  >NUL 2>&1".format(firewall_rule_name, x2)
+                        os.system(cmd)
+                        
+                    except:
+                        pass 
 
+            elif toblk[key] == False :
+                    try:
+                        cmd = "netsh advfirewall firewall delete rule  name={0} >NUL 2>&1 ".format(firewall_rule_name)
+                        os.system(cmd)
+                    except:
+                        pass             
+        toblk = {}
+
+
+
+
+### Functionn to fetch states of all checkboxed and load them into a toblk (to block ) dictionary : True/False
+    def newFncStart(self):
+        global sv_list 
+        global toblk
+        toblk = {}
+        checkboxes = self.ui.mainFrame.findChildren(QCheckBox)
+
+        for checkbox in checkboxes:
+            toblk[checkbox.objectName()] = checkbox.isChecked()
+        self.newBlockFnc()
+    
+
+
+   
+
+
+
+
+
+
+### Pre-Checks all already blocked servers  and reapply settings
     def fetchCurrBlocked(self):
         global sv_list
         global currBlocked
-        tmplist  = []
+        global toblk
+        toblk = {}
         for x in sv_list:
             cmd = "netsh advfirewall firewall show rule name=CS2ServerSelect_BLOCK_{0} >NUL 2>&1".format(x)
             if os.system(cmd) == 1:
+                toblk[x] = False
                 try:
-                    tmplist.pop(x)
+                    currBlocked.pop(x)
                 except:
                     pass
             elif os.system(cmd) == 0:
-                tmplist.append(x) 
-
-        currBlocked = set(tmplist)        
+                toblk[x] = True
+                currBlocked.append(x) 
+        currBlocked = set(currBlocked)  
+        
+        self.unblock()
+     
+        tmp_thread = threading.Thread(target=self.newBlockFnc)
+        tmp_thread.start()      
         return currBlocked
 
     def onload(self):
         global currBlocked 
         global alldata
         global sv_list
-        ## trying to fetch data from api
 
+    
         url  = "https://api.steampowered.com/ISteamApps/GetSDRConfig/v1/?appid=730"
 
 
@@ -272,57 +208,28 @@ class MainWindow(QMainWindow):
             try:
                 while x < len(t2[x1]["relays"]):
                     ips.append(t2[x1]["relays"][x]["ipv4"])
-                    alldata[x1] = ips
-                    
                     x = x +1
+                alldata[x1] = ips        
             except:
                 pass
         
         self.fetchCurrBlocked()
+        time.sleep(0.25)
+ 
+        checkboxes = self.ui.mainFrame.findChildren(QCheckBox)
+        self.ui.progressBar.setValue(50)
+        
+        for checkbox in checkboxes:
+            time.sleep(0.005)
+            if checkbox.objectName() in currBlocked:
+                checkbox.setCheckState(Qt.Checked)
+                
+                
 
-        if "bom" in currBlocked:
-            self.ui.checkBox.setCheckState(Qt.Checked)
-        if "bom2" in currBlocked:
-            self.ui.checkBox_2.setCheckState(Qt.Checked)
-        if "maa" in currBlocked:
-            self.ui.checkBox_3.setCheckState(Qt.Checked)
-        if "maa2" in currBlocked:
-            self.ui.checkBox_4.setCheckState(Qt.Checked)
-        if "canm" in currBlocked:
-            self.ui.checkBox_5.setCheckState(Qt.Checked)
-        if "ctum" in currBlocked:
-            self.ui.checkBox_6.setCheckState(Qt.Checked)
-        if "sham" in currBlocked:
-            self.ui.checkBox_7.setCheckState(Qt.Checked)
-        if "tsnm" in currBlocked:
-            self.ui.checkBox_8.setCheckState(Qt.Checked)
-        if "ams" in currBlocked:
-            self.ui.checkBox_9.setCheckState(Qt.Checked)
-        if "atl" in currBlocked:
-            self.ui.checkBox_10.setCheckState(Qt.Checked)
-        if "fra" in currBlocked:
-            self.ui.checkBox_11.setCheckState(Qt.Checked)
-        if "lhr" in currBlocked:
-            self.ui.checkBox_12.setCheckState(Qt.Checked)
-        if "par" in currBlocked:
-            self.ui.checkBox_13.setCheckState(Qt.Checked)
-        if "mad" in currBlocked:
-            self.ui.checkBox_14.setCheckState(Qt.Checked)
-        if "dfw" in currBlocked:
-            self.ui.checkBox_15.setCheckState(Qt.Checked)
-        if "jfk" in currBlocked:
-            self.ui.checkBox_16.setCheckState(Qt.Checked)
-        if "iad" in currBlocked:
-            self.ui.checkBox_17.setCheckState(Qt.Checked)
-        if "sea" in currBlocked:
-            self.ui.checkBox_18.setCheckState(Qt.Checked)
-        if "dxb" in currBlocked:
-            self.ui.checkBox_19.setCheckState(Qt.Checked)
-        if "sgp" in currBlocked:
-            self.ui.checkBox_20.setCheckState(Qt.Checked)
-        if "seo" in currBlocked:
-            self.ui.checkBox_21.setCheckState(Qt.Checked)
-        self.ui.stackedWidget.setCurrentIndex(0)
+        self.ui.progressBar.setValue(100)
+        time.sleep(0.9)
+        self.ui.progressBar.hide()
+        self.ui.stackedWidget.setCurrentIndex(1)
         
         
 
